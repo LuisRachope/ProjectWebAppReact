e1d2d257ab537808b3faeab7b389553835a88865
Luis Rachope <luis.chiasso@hotmail.com>
2023-02-26 16:22:32 -0300

    Add project files.

---
+++ ProjectWebAppReact/Data/ProjectWebAppReactDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectWebAppReact.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectWebAppReact.Data
+{
+    public class ProjectWebAppReactDbContext : DbContext
+    {
+        public ProjectWebAppReactDbContext(DbContextOptions<ProjectWebAppReactDbContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
