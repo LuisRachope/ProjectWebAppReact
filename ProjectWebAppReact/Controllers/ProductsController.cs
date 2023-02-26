a3b2d4bd9fbcc3ea831ea604e32511c5d6c20350
Luis Rachope <luis.chiasso@hotmail.com>
2023-02-26 16:33:53 -0300

    Creação do controller de produtos e funções de getall e get by id

---
+++ ProjectWebAppReact/Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProjectWebAppReact.Data;
+using ProjectWebAppReact.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProjectWebAppReact.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProjectWebAppReactDbContext _projectWebAppReactDbContext;
+
+        public ProductsController(ProjectWebAppReactDbContext projectWebAppReactDbContext)
+        {
+            _projectWebAppReactDbContext = projectWebAppReactDbContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAll()
+        {
+            return await _projectWebAppReactDbContext.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetById(int id)
+        {
+            var product = await _projectWebAppReactDbContext.Products.FindAsync(id);
+
+            if (product is null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
